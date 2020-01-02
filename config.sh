8500c002b62caf080c0f7ec1fe52d4ad9f91ddc7
notorso <notorso@protonmail.com>
2020-01-02 10:46:03 +0100

    vscode snap

--- config.sh
+++ config.sh
@@ -5,6 +5,10 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # git stuff
 git config --global user.email "notorso@protonmail.com"
 git config --global user.name "notorso"
+
 #install zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#snaps (they really really suck)
+snap install code
