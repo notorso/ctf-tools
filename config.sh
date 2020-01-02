e474e22bb0b2dc6aa7474119df514443566a1553
notorso <notorso@protonmail.com>
2020-01-02 11:00:07 +0100

    zsh shell def

--- config.sh
+++ config.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # git stuff
 git config --global user.email "notorso@protonmail.com"
 git config --global user.name "notorso"
 
 #install zsh plugins
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh)
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
