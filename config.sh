023f7806e41b41db6eb4190419bdc081834e1a2d
notorso <notorso@protonmail.com>
2020-01-02 10:11:57 +0100

    git config

--- config.sh
+++ config.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# git stuff
+git config --global user.email "notorso@protonmail.com"
+git config --global user.name "notorso"
+
