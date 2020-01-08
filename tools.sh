055fc09ba01526bb833d79ae4dc5ca9579c22e80
notorso <notorso@protonmail.com>
2020-01-08 13:32:21 +0100

    changed dir

--- tools.sh
+++ tools.sh
@@ -1,33 +1,20 @@
-mkdir /home/$(whoami)/tools
+#!/bin/bash -ex
+mkdir ~/tools
 #snaps (they really really suck)
-echo -n "----------"
-echo -n "just only one snap, vscode"
-echo -n "----------"
-
-snap install vscode --classic
+sudo snap install vscode --classic
 
 #stegsolve
-echo -n "----------"
-echo -n "installing Stegsolve"
-echo -n "----------"
 wget http://www.caesum.com/handbook/Stegsolve.jar -O stegsolve.jar
 chmod +x stegsolve.jar
 mv stegsolve.jar ~/tools
 
 #johntheripper
-echo -n "----------"
-echo -n "installing JohnTheRipper"
-echo -n "----------"
-apt -y install libssl-dev libz3-4
-git clone https://github.com/magnumripper/JohnTheRipper home/$(whoami)/tools/jtr
-
+sudo apt -y install libssl-dev libz3-4
+git clone https://github.com/magnumripper/JohnTheRipper ~/tools/jtr 
 echo -e '\033[now ./configure && make jtr in /src, [ENTER] to continue:\033[0m'
 read var_name
 
 #rockyou
-echo -n "----------"
-echo -n "getting rockyou.txt"
-echo -n "----------"
 wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt -O ~/tools/rockyou.txt
 
 
